cf797d1c33e6e79101ec9e5efebfb080fcd0f62e
Mohamed2Bahaa <moh2003299@gmail.com>
2024-11-20 21:57:19 +0200

    adding ProductRequest record

--- services/product/src/main/java/com/mohamed/ecommerce/product/ProductRequest.java
+++ services/product/src/main/java/com/mohamed/ecommerce/product/ProductRequest.java
@@ -1,4 +1,21 @@
 package com.mohamed.ecommerce.product;
 
-public record ProductRequest() {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record ProductRequest(  Integer id,
+      @NotNull(message = "Product name is required")
+      String name,
+      @NotNull(message = "description is required")
+      String description,
+      @Positive(message = "available_quantity should be positive")
+      double available_quantity,
+      @Positive(message = "price should be positive")
+      BigDecimal price,
+      @NotNull(message = "price cateogry is required")
+      Integer categoryId
+ ) {
+
 }
