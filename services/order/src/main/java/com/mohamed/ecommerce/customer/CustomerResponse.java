6570fa071dcd1a42df434446c4051c1fe5b40d1d
Mohamed2Bahaa <moh2003299@gmail.com>
2024-11-23 22:18:41 +0200

    record

---
+++ services/order/src/main/java/com/mohamed/ecommerce/customer/CustomerResponse.java
@@ -0,0 +1,12 @@
+package com.mohamed.ecommerce.customer;
+
+public record CustomerResponse(
+        String id,
+        String firstname,
+        String lastname,
+         String email
+)
+{
+
+
+}
