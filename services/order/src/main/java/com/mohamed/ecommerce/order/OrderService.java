2a02d76d8393ffbc604479a51f7ec6bcd8369490
Mohamed2Bahaa <moh2003299@gmail.com>
2024-11-23 22:18:20 +0200

    checking if  the customer exists

--- services/order/src/main/java/com/mohamed/ecommerce/order/OrderService.java
+++ services/order/src/main/java/com/mohamed/ecommerce/order/OrderService.java
@@ -1,6 +1,8 @@
 package com.mohamed.ecommerce.order;
 
 import com.mohamed.ecommerce.customer.CustomerClient;
+import com.mohamed.ecommerce.exception.BusinessException;
+import com.mohamed.ecommerce.product.ProductClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +10,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class OrderService {
     private final CustomerClient customerClient;
+    private final ProductClient productClient;
     public Integer createOrder(OrderRequest request) {
-        //check custoemr
+        var customer =this.customerClient.findCustomerById(request.customerId())
+                .orElseThrow(()->new BusinessException("Cannot create order :: No Customer exists" +
+                        "with the provided ID ::"+ request.customerId()));
 
         //purchase the product
 
