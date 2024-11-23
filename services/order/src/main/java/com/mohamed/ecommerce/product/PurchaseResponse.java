11123200cf0f9e7e753449116a66e7ab3568a179
Mohamed2Bahaa <moh2003299@gmail.com>
2024-11-23 22:15:15 +0200

    records

---
+++ services/order/src/main/java/com/mohamed/ecommerce/product/PurchaseResponse.java
@@ -0,0 +1,19 @@
+package com.mohamed.ecommerce.product;
+
+import java.math.BigDecimal;
+
+public record PurchaseResponse(
+
+        Integer productId,
+
+        String name,
+
+        String description,
+
+        BigDecimal price,
+
+        double quantity
+
+)
+{
+}
