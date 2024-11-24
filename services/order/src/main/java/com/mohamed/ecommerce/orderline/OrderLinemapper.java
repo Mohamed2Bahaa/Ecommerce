b650b1e3d5cc4d79704b4be247b2b57b443d27ac
Mohamed2Bahaa <moh2003299@gmail.com>
2024-11-24 04:25:08 +0200

    adding OrderLinemapper (toOrderline function)

---
+++ services/order/src/main/java/com/mohamed/ecommerce/orderline/OrderLinemapper.java
@@ -0,0 +1,19 @@
+package com.mohamed.ecommerce.orderline;
+
+import com.mohamed.ecommerce.order.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderLinemapper {
+    public OrderLine toOrderLine(OrderLineRequest request) {
+
+        return OrderLine.builder().
+                id(request.id()).
+                quantity(request.quantity())
+                        .order(
+                                Order.builder().id(request.id())
+                                        .build()
+                        ).productId(request.productId()).
+                build();
+    }
+}
