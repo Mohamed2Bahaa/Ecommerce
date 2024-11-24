e2ccfb6357758c2073042c033b537aa04114c851
Mohamed2Bahaa <moh2003299@gmail.com>
2024-11-24 04:18:49 +0200

    saving orderLines

--- services/order/src/main/java/com/mohamed/ecommerce/order/OrderService.java
+++ services/order/src/main/java/com/mohamed/ecommerce/order/OrderService.java
@@ -2,7 +2,10 @@ package com.mohamed.ecommerce.order;
 
 import com.mohamed.ecommerce.customer.CustomerClient;
 import com.mohamed.ecommerce.exception.BusinessException;
+import com.mohamed.ecommerce.orderline.OrderLineRequest;
+import com.mohamed.ecommerce.orderline.OrderLineService;
 import com.mohamed.ecommerce.product.ProductClient;
+import com.mohamed.ecommerce.product.PurchaseRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,7 @@ public class OrderService {
     private final OrderRepository repository;
     private final CustomerClient customerClient;
     private final ProductClient productClient;
+    private final OrderLineService orderLineService;
     public Integer createOrder(OrderRequest request) {
         var customer =this.customerClient.findCustomerById(request.customerId())
                 .orElseThrow(()->new BusinessException("Cannot create order :: No Customer exists" +
@@ -23,7 +27,21 @@ public class OrderService {
 
         //persist order
         var order = this.repository.save(mapper.toOrder(request));
+
         //persist order lines
+        for (PurchaseRequest purchaseRequest: request.products())
+        {
+            orderLineService.saveOrderLine(
+                    new OrderLineRequest
+                    (
+                    null,
+                     order.getId(),
+                     purchaseRequest.productId(),
+                     purchaseRequest.quantity()
+                    )
+            );
+
+        }
 
         //start payment
 
