238c43260e59cde49728e3b3e93df254c3e8c05d
Mohamed2Bahaa <moh2003299@gmail.com>
2024-11-24 06:05:49 +0200

    sending order confirmation part

--- services/order/src/main/java/com/mohamed/ecommerce/order/OrderService.java
+++ services/order/src/main/java/com/mohamed/ecommerce/order/OrderService.java
@@ -2,6 +2,8 @@ package com.mohamed.ecommerce.order;
 
 import com.mohamed.ecommerce.customer.CustomerClient;
 import com.mohamed.ecommerce.exception.BusinessException;
+import com.mohamed.ecommerce.kafka.OrderConfirmation;
+import com.mohamed.ecommerce.kafka.OrderProducer;
 import com.mohamed.ecommerce.orderline.OrderLineRequest;
 import com.mohamed.ecommerce.orderline.OrderLineService;
 import com.mohamed.ecommerce.product.ProductClient;
@@ -17,13 +19,14 @@ public class OrderService {
     private final CustomerClient customerClient;
     private final ProductClient productClient;
     private final OrderLineService orderLineService;
+    private final OrderProducer orderProducer;
     public Integer createOrder(OrderRequest request) {
         var customer =this.customerClient.findCustomerById(request.customerId())
                 .orElseThrow(()->new BusinessException("Cannot create order :: No Customer exists" +
                         "with the provided ID ::"+ request.customerId()));
 
         //purchase the product
-        this.productClient.purchaseProducts(request.products());
+     var purchasedProducts = this.productClient.purchaseProducts(request.products());
 
         //persist order
         var order = this.repository.save(mapper.toOrder(request));
@@ -42,10 +45,19 @@ public class OrderService {
             );
 
         }
-
-        //start payment
+        //todo start payment
 
         //send the order confirmation
-        return null;
+
+        orderProducer.sendOrderConfirmation(
+                new OrderConfirmation(
+                        request.reference(),
+                        request.amount(),
+                        request.PaymentMethod(),
+                        customer,
+                        purchasedProducts
+                )
+        );
+        return order.getId();
     }
 }
