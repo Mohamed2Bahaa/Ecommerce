efa3d73291ed9cec0cda1e58b134f9ce1450acac
Mohamed2Bahaa <moh2003299@gmail.com>
2024-11-26 01:19:38 +0200

    adding payment entity

---
+++ services/payment/src/main/java/com/mohamed/ecommerce/payment/Payment.java
@@ -0,0 +1,24 @@
+package com.mohamed.ecommerce.payment;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "payment")
+public class Payment {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private BigDecimal  amount;
+    private PaymentMethod paymentMethod;
+
+}
