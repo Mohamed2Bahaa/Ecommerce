d72cb219868106f0a49433b8d95bee061cf6fe3f
Mohamed2Bahaa <moh2003299@gmail.com>
2024-11-26 01:24:29 +0200

    adding vars of the entity

--- services/payment/src/main/java/com/mohamed/ecommerce/payment/Payment.java
+++ services/payment/src/main/java/com/mohamed/ecommerce/payment/Payment.java
@@ -2,9 +2,14 @@ package com.mohamed.ecommerce.payment;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.EnumType.STRING;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,6 +24,15 @@ public class Payment {
     @GeneratedValue
     private Integer id;
     private BigDecimal  amount;
+    @Enumerated(STRING)
     private PaymentMethod paymentMethod;
+    private Integer orderId;
+    @CreatedDate
+    @Column(updatable = false,nullable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
 
 }
