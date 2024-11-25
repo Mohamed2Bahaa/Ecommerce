3f42b59ad586ae85fbbb408f6c3d353504663a3e
Mohamed2Bahaa <moh2003299@gmail.com>
2024-11-26 00:30:05 +0200

    DB connection

--- services/order/src/main/java/com/mohamed/ecommerce/orderline/OrderLineRepository.java
+++ services/order/src/main/java/com/mohamed/ecommerce/orderline/OrderLineRepository.java
@@ -2,6 +2,9 @@ package com.mohamed.ecommerce.orderline;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderLineRepository extends JpaRepository<OrderLine,Integer> {
 
+    List<OrderLine> findAllByOrderId(Integer orderId);
 }
