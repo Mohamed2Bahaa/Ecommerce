3e3c7a76800bdf9aef86dcbb7bb674f72cc7b4c7
Mohamed2Bahaa <moh2003299@gmail.com>
2024-11-26 02:42:12 +0200

    adding  create payment function

--- services/payment/src/main/java/com/mohamed/ecommerce/payment/PaymentService.java
+++ services/payment/src/main/java/com/mohamed/ecommerce/payment/PaymentService.java
@@ -6,4 +6,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
+    private final PaymentRepository repository;
+    private final PaymentMapper mapper;
+    public Integer createPayment(PaymentRequest request) {
+        var payment= repository.save(mapper.toPayment(request));
+   return null;
+    }
 }
